2af035405a7a0756968827bdcc16bc91c6c994cb
sarojvrc <saroj.vrc@gmail.com>
2021-12-02 20:14:51 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
-import TodoList from './components/TodoList';
+import TodoList from './Components/TodoList';
+import Footer from './Components/Footer';
 
 function App() {
   return (
     <div className='todo-app'>
       <TodoList />
+      
     </div>
   );
 }
